9e4c6d22b694fde86f8c20d2e12e661bbd5388dc
John Liu <johnliu478@gmail.com>
2019-11-07 10:50:36 -0500

    Add prediction functionality to classifier

--- classifier.py
+++ classifier.py
@@ -6,25 +6,26 @@ from joblib import dump as export_weights, load as load_weights
 from os.path import expanduser
 from sklearn.svm import OneClassSVM
 from keras.preprocessing import image
+from PIL import Image as PILImage
 
-weights_file_name = 'svm_weights.joblib'
-target_size = (150, 150)
 home = expanduser('~')
+weights_file_name = f'{home}/Google Drive/tcv/svm_weights.joblib'
+target_size = (150, 150)
 svm = OneClassSVM(gamma='auto')
 
 def train():
     global svm
 
-    num_augmentations = 6
+    num_augmentations = 1
     x_train = []
     for i in range(175):
         print(f'Loading light_{i}.jpg')
         imgs = augment.generate(f'{home}/Google Drive/tcv/images/traffic_lights/light_{i}.png', target_size=target_size, num_imgs=num_augmentations)
-        predictions = []
+        conv_outputs = []
         for j in range(num_augmentations):
-            prediction = convnet.predict(imgs[j].reshape((1,) + imgs[j].shape))
-            predictions.append(prediction)
-        x_train.extend(predictions)
+            conv_output = convnet.predict(imgs[j].reshape((1,) + imgs[j].shape))
+            conv_outputs.append(conv_output)
+        x_train.extend(conv_outputs)
 
     x_train = np.asarray(x_train)
     svm.fit(x_train)
@@ -33,5 +34,18 @@ def train():
 def load():
     global svm
     svm = load_weights(weights_file_name)
+    img = PILImage.open(f'{home}/Desktop/test_image.png')
+    img = img.convert('RGB')
+    img = img.resize((150, 150))
+    img = image.img_to_array(img)
+    img = img.reshape((1,) + img.shape)
+    img /= 255.0
+
+    conv_output = convnet.predict(img)
+    conv_output = conv_output.reshape((1,) + conv_output.shape)
+
+    y = svm.predict(conv_output)
+    print(y)
+
 
 load()
